0fa29461a7a6f416b150725e68f47c1ea9a84b39
Matt Gregg <matt.gregg@shopify.com>
2024-07-03 20:40:31 -0500

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,liquid}'],
+  content: ["./src/**/*.{html,liquid}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+        display: ["Italiana", "sans-serif"],
+        customFont: ["Italiana", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
